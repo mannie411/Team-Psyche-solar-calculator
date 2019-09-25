cf6da4130aabbecfba431e20983cae3e78515615
Emmanuel Imoh <imohyo@gmail.com>
2019-09-25 12:38:10 +0000

    Refactored the app and implemented the index page.

---
+++ frontend/src/index.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { BrowserRouter as Router, Route, Switch   } from 'react-router-dom'
+import {createBrowserHistory} from 'history'
+
+// layout
+import MainLayout from './layouts/Main';
+
+// views
+import App from './views/App';
+
+// Assets
+import './assets/css/reset.css';
+import './assets/css/tachyons.min.css'
+import './assets/css/App.css';
+
+ 
+// ServiceWorker
+import * as serviceWorker from './serviceWorker';
+
+const history = createBrowserHistory();
+
+ReactDOM.render(
+                    <Router history={history} >
+                    
+                      <Switch>
+                        <MainLayout>
+                          <Switch>
+                          <Route exact path="/" component={App} />
+                          
+                          </Switch>
+                        </MainLayout>
+                        
+                      </Switch>
+           
+                    </Router>
+               ,
+                 document.getElementById('root'));
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: http://bit.ly/CRA-PWA
+serviceWorker.unregister();
---
+++ frontend/src/layouts/Main.js
@@ -0,0 +1,21 @@
+import React from 'react'
+
+
+
+
+
+
+
+
+
+const Main = (props) => {
+
+  return (
+    <div >
+           {props.children}
+    </div>
+   
+  )
+}
+
+export default Main
