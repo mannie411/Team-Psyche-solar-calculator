d984d7f36c934845316b9d3ac3e40dd229cb6b55
Emmanuel Imoh <imohyo@gmail.com>
2019-09-26 00:51:18 +0000

    added some basic styling

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter as Router, Route, Switch   } from 'react-router-dom'
+import { Router, Route, Switch   } from 'react-router-dom'
 import {createBrowserHistory} from 'history'
 
 // layout
--- src/views/App.js
+++ src/views/App.js
@@ -1,13 +1,51 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Logo from '../assets/images/Logo.svg'
 import Thunderbolt from '../assets/images/Thunderbolt.svg'
 
 
-function App() {
 
-  const showModal = () => {
-      console.log('you have clicked this button')
+
+
+class App extends Component {
+
+  
+  componentDidMount(){
+    this.triggerModal();
   }
+
+  triggerModal(){
+    // Get the modal
+    var modal = document.getElementById('myModal');
+
+    // Get the button that opens the modal
+    var btn = document.getElementById("myBtn");
+
+    // Get the <span> element that closes the modal
+    var close = document.getElementById("close");
+
+    // When the user clicks the button, open the modal 
+    btn.onclick = function() {
+        modal.style.display = "block";
+    }
+
+    // When the user clicks on <span> (x), close the modal
+    close.onclick = function() {
+        modal.style.display = "none";
+    }
+
+    // When the user clicks anywhere outside of the modal, close it
+    window.onclick = function(event) {
+        if (event.target === modal) {
+            modal.style.display = "none";
+        }
+    }
+  }
+
+  handleSubmit(){
+    console.log('calculating')
+  }
+
+  render() {  
 
   return (
     <div className="App">
@@ -19,55 +57,98 @@ function App() {
       </header>
       <main>
       <div className="mw9 center ph3-ns">
-        <div className="cf ph2-ns">
-          <div className="fl w-100 w-50-ns pa2">
-            <div className="">
-              <div className='add-form bg-white'>
-              <div className="">
-                  <div>
+        
+          <div className="fl add-form bg-white mobile">
+           
+              <div className=' '>
+             
+                  <div className='ma3 tc'>
                     <h1>Add an appliance</h1>
                   </div>
                   <div>
-                    <div>
-                      <label>Name of appliance</label>
-                      <input type='text' placeholder='Televsion' />
+                    <div className='ma3'>
+                      <label className=''>Name of appliance</label>
+                      <input  type='text' placeholder='Televsion' />
                     </div>
                     <div className='' >
-                      <div className='' >
-                      <div>
+                      <div className='ma3 dib'>
                         <label>Number of Appliance</label>
                         <input type='number' placeholder='1' />
                       </div>
-                      <div>
-                        <label>Energy</label>
+                      <div className='ma3 dib'>
+                        <label>Time  <span>Hr</span></label>
                         <input type='number' placeholder='1' />
-                        <span>Watt</span>
+                       
                       </div>
-
+                      <div className='ma3 dib'>
+                        
+                        <label>Energy <span>Watt</span> </label>
+                        <input type='number' placeholder='1' />
                       </div>
-
                     </div>
-                   
-                    
-
                   </div>
-
                   <div>
-
-                    <button className='btn-add'>Add  Appliance</button>
+                    <button className='btn-add' type='submit' onClick={this.handleSubmit}>Add  Appliance</button>
                   </div>
               </div>
               </div>
+
+              <div id="myModal" className="modal">
+
+               
+                <div className="modal-content">
+                 
+                  <div className=' '>
+             
+             <div className='ma3 tc'>
+               <h1>Add an appliance</h1>
+             </div>
+             <div>
+               <div className='ma3'>
+                 <label className=''>Name of appliance</label>
+                 <input  type='text' placeholder='Televsion' />
+               </div>
+               <div className='' >
+                 <div className='ma3 dib'>
+                   <label>Number of Appliance</label>
+                   <input type='number' placeholder='1' />
+                 </div>
+                 <div className='ma3 dib'>
+                   <label>Time  <span>Hr</span></label>
+                   <input type='number' placeholder='1' />
+                  
+                 </div>
+                 <div className='ma3 dib'>
+                   
+                   <label>Energy <span>Watt</span> </label>
+                   <input type='number' placeholder='1' />
+                 </div>
+               </div>
+             </div>
+             <div>
+               <button className='btn-add' type='submit' onClick={this.handleSubmit}>Add  Appliance</button>
+             </div>
+
+             <div>
+               <button id='close' href='javascript;;'>Close</button>
+             </div>
+         </div>
+                </div>
+
+               </div>
+              
              
 
             </div>
 
-          </div>
-          <div className='calc-info bg-white fl w-100 w-50-ns pa2' id="modal-2">
+          
+          <div className='calc-info bg-white fl '>
             <div className=''>
               <div className='calc-total'>
-                <div className="dib mr4 ml4">
+                <div className="mr4 ml4 mt3">
                   <h3 className="ttu tracked">Calculations</h3>
+                </div>
+                <div className="dib mr4 ml4">  
                   <h4>Total Consumption</h4>
                 </div>
                 <div className="dib mr4 ml4"> 
@@ -76,20 +157,59 @@ function App() {
                 <div className="dib mr4 ml4">300<span>Wh/day</span></div>
               </div>
               <div className='calc-data'>
-
+                <div className='ma3'>
+                  <h3 className='ttu'>Appliance List</h3>
+                </div>
+                <div className='ma3'>
+                <table>
+                  <thead>
+                  <tr>
+                    <th>Name</th>
+                    <th>Number</th>
+                    <th>Time</th>
+                    <th>Energy</th>
+                  </tr>
+                  </thead>
+                 
+                  <tbody>
+                  <tr>
+                    <td>Cooker</td>
+                    <td>1</td>
+                    <td>2</td>
+                    <td>20</td>
+                  </tr>
+                  <tr>
+                    <td>AC Conditioner</td>
+                    <td>5</td>
+                    <td>10</td>
+                    <td>70</td>
+                  </tr>
+                  <tr>
+                    <td>Radio</td>
+                    <td>2</td>
+                    <td>5</td>
+                    <td>10</td>
+                  </tr>
+                  </tbody>
+                  
+                 
+                </table>
+                </div>
+                
               </div>
             </div>
           </div>
-        </div>
-      </div>
+       
+    
 
       <div >
-        <button className='btn-add-circle' onClick={showModal}>+</button>
-       
+        <button id="myBtn" className='btn-add-circle' >+</button>
+
       </div>
       </main>
     </div>
   );
+}
 }
 
 export default App;
